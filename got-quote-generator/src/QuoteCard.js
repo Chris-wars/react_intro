457355f9809976bce2cc2ec9fd52d40aa1369c40
Chris-wars <bloodynova88@gmail.com>
2025-08-20 11:58:50 +0200

    Refact

--- got-quote-generator/src/QuoteCard.js
+++ got-quote-generator/src/QuoteCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-function QuoteCard(props) {
+function QuoteCard({ quoteText, characterName, isQuoteEpic }) {
     return (
         <blockquote className="quote-card">
-            "{props.quoteText}"
+            "{quoteText}"
             <footer className="quote-character">
-                - {props.characterName}
-                {props.isQuoteEpic && <span style={{ marginLeft: '10px' }}>🌟</span>}
+                - {characterName}
+                {isQuoteEpic && <span style={{ marginLeft: '10px' }}>🌟</span>}
             </footer>
         </blockquote>
     )
